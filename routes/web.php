376bec667e5ab784aee096ec2d6c3c734fd7f3f9
ZEINZH <91441817+suryanicholas@users.noreply.github.com>
2024-05-29 20:23:24 +0700

    Init Soft UI
    Gratisan :v

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,34 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('dashboard', function(){
+    return view('dashboard.index');
+});
+
+Route::get('dashboard/tables', function(){
+    return view('dashboard.tables');
+});
+
+Route::get('dashboard/billing', function(){
+    return view('dashboard.billing');
+});
+
+Route::get('dashboard/virtual-reality', function(){
+    return view('dashboard.virtual-reality');
+});
+
+Route::get('dashboard/rtl', function(){
+    return view('dashboard.rtl');
+});
+
+Route::get('profile', function(){
+    return view('profile');
+});
+
+
+Route::get('signin', function(){
+    return view('authorize.signin');
 });
