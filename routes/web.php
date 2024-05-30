c04c18fcf8357ef62742de89b91356f34d9128b2
ZEINZH <91441817+suryanicholas@users.noreply.github.com>
2024-05-30 15:12:10 +0700

    Other

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,6 @@ Route::get('dashboard/billing', function(){
     return view('dashboard.billing');
 });
 
-Route::get('dashboard/virtual-reality', function(){
-    return view('dashboard.virtual-reality');
-});
-
-Route::get('dashboard/rtl', function(){
-    return view('dashboard.rtl');
-});
-
 Route::get('profile', function(){
     return view('profile');
 });
